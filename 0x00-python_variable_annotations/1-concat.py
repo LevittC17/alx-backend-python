280eaba4f8db3dd7bdcf26441fb19a02993160f2
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-06 18:05:25 +0000

    added documentation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+"""type-annotated function that taes str1, str2
+as arguments"""
+
+
 def concat(str1: str, str2: str) -> str:
     """Cocatenate two strings and return the result as a string"""
     return str1 + str2
