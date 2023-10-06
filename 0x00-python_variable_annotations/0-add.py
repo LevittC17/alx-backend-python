03a12dda81df91d10c008ba25bdd4e1beb0e1613
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-06 17:44:04 +0000

    take 2 floats as arguments and return their sum as a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Function add() that takes two float arguments
+`a` and `b`, returns their sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return the result as a float"""
+    return a + b
