6c7fb9380db77b671447cbd8c96ae683c9d0de07
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-06 18:11:25 +0000

    return the floor of a float as an integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+type-annotated function - floor()
+two arguments and returns the floor of the float
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float as an integer"""
+    return math.floor(n)
