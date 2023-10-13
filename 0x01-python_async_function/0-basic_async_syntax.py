40e61eb1a947a4e0436e42c48569fa46b68467d3
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-13 21:11:35 +0000

    vriables and return value have been annotated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,8 +9,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """wait for a random delay between 1 and 10"""
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
