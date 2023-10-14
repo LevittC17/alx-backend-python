0b87feb0e5a312608a51e66508a25272f0a69d15
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-14 21:54:23 +0000

    annotated the function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,9 +11,10 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
-    '''An asynchronous generator that yields random
-    numbers between 0 and 10'''
+    """An asynchronous generator that yields
+    random numbers between 0 and 10."""
     for _ in range(10):
-        await asyncio.sleep(1)  # wait for 1 second asynchronously
+        await asyncio.sleep(1)  # Wait for 1 second asynchronously
         yield random.uniform(0, 10)
