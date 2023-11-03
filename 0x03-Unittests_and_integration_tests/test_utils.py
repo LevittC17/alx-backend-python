9081fddf91087fc269e46776fc8e383404608674
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-03 11:40:24 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,9 @@
 
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 from utils import access_nested_map
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -36,6 +38,31 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        with patch('requests.get') as mock_get:
+            # Create a mock response object with a json method
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+
+            # Configure the mock get method to return the mock_response
+            mock_get.return_value = mock_response
+
+            # Call the get_json function
+            result = get_json(test_url)
+
+            # Ensure that the get method was called exactly once
+            # with the test_url
+            mock_get.assert_called_once_with(test_url)
+
+            # Test that the output of get_json is equal to test_payload
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
